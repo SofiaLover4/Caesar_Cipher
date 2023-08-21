d29124d2772f29a334a8f657914005098f14299e
Adrian Perez <adrian1804perez@gmail.com>
2023-08-20 20:00:28 -0700

    remove other spaces

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -12,10 +12,9 @@ def caesar_cipher(sentence, shift)
     elsif (c.ord >= 97  && c.ord <= 122)
       num = c.ord + shift <= 122 ? c.ord + shift : ((c.ord + shift) - 122) + 96 # number will loop back down if it goes above 122
       new_sentence += num.chr
-    else 
+    else
       new_sentence += c
-    end 
+    end
   }
   new_sentence
 end
-
