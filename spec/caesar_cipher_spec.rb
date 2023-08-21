e045715a47871b000a8692edb9064d643522ded5
Adrian Perez <adrian1804perez@gmail.com>
2023-08-20 19:17:54 -0700

    Add test for capitalization

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -6,4 +6,8 @@ describe '#caesar_cipher' do
     expect(caesar_cipher('hello there', 3)).to eql('khoor wkhuh')
   end
 
+  it 'Capitalization stays the same' do
+    expect(caesar_cipher('How is it going', 4)).to eql('Lsa mw mx ksmrk')
+  end
+
 end
