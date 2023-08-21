799fe42bb720b00cf1030fde4de9d950a10a4998
Adrian Perez <adrian1804perez@gmail.com>
2023-08-20 19:59:48 -0700

    adjust quotation marks and spece before paremeters

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -2,9 +2,9 @@
 # Upper case letter range is from 65 - 90
 
 
-def caesar_cipher (sentence, shift)
-  new_sentence = ""
-  sentence.each_char{
+def caesar_cipher(sentence, shift)
+  new_sentence = ''
+  sentence.each_char {
     |c|
     if (c.ord >= 65 && c.ord <= 90)
       num = c.ord + shift <= 90 ? c.ord + shift : ((c.ord + shift) - 90) + 64 # If the number goes above 90 it will loop back down to 65  
