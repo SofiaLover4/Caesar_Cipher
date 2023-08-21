6e9fa482a88e8db50ab7b3f43308ab0190c7a19c
Adrian Perez <adrian1804perez@gmail.com>
2023-08-20 20:03:33 -0700

    Create new variable for the c.ord

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -5,11 +5,12 @@
 def caesar_cipher(sentence, shift)
   new_sentence = ''
   sentence.each_char do |c|
-    if (c.ord >= 65 && c.ord <= 90)
-      num = c.ord + shift <= 90 ? c.ord + shift : ((c.ord + shift) - 90) + 64 # If the number goes above 90 it will loop back down to 65  
+    letter_num = c.ord
+    if (letter_num >= 65 && letter_num <= 90)
+      num = letter_num + shift <= 90 ? letter_num + shift : ((letter_num + shift) - 90) + 64 # If the number goes above 90 it will loop back down to 65  
       new_sentence += num.chr
-    elsif (c.ord >= 97  && c.ord <= 122)
-      num = c.ord + shift <= 122 ? c.ord + shift : ((c.ord + shift) - 122) + 96 # number will loop back down if it goes above 122
+    elsif (letter_num >= 97  && letter_num <= 122)
+      num = letter_num + shift <= 122 ? letter_num + shift : ((letter_num + shift) - 122) + 96 # number will loop back down if it goes above 122
       new_sentence += num.chr
     else
       new_sentence += c
