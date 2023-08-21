f0dac07ce6791f41ecad8a2d32b1fdee22df9b22
Adrian Perez <adrian1804perez@gmail.com>
2023-08-20 19:41:30 -0700

    Add test that makes sure punctuation is not changed

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -10,4 +10,7 @@ describe '#caesar_cipher' do
     expect(caesar_cipher('How is it going', 4)).to eql('Lsa mw mx ksmrk')
   end
 
+  it 'punctuation is not changed' do
+    expect(caesar_cipher('However, this is false!', 4)).to eql('Lsaiziv, xlmw mw jepwi!')
+  end
 end
